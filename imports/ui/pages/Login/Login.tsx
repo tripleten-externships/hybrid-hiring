5ce08bb5f84dec78cfc80dfb4b6d3fa87541f659
RyanStoll <pushthatboard@gmail.com>
2026-03-01 22:53:37 -0600

    [HH-56] Add outline for login page

---
+++ imports/ui/pages/Login/Login.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Layout } from '../components/Layout.tsx';
+import "./Login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+return (
+    <Layout>
+<form className='login__form' >
+    <h2 className="login__form-title">Welcome back or LogIn to your account</h2>
+    <label htmlFor='email' className='login__form-label'>Email</label>
+    <input 
+        type='email'
+        id='email'
+        className='login__form-input'
+        value={email}
+        placeholder='Enter your email'
+        onChange={e => setEmail(e.target.value)}
+        required
+    />
+    <label htmlFor='password' className='login__form-label'>Password</label>
+    <input 
+        type={showPassword ? 'text' : 'password'}
+        id='password'
+        className='login__form-input'
+        value={password}
+        placeholder='Enter your password'
+        onChange={e => setPassword(e.target.value)}
+        required
+    />
+</form>
+    <h3 className="login__signup-btn">Don't have an account? <a href="/signup">Sign up</a></h3>
+    </Layout>
+    );
+};
+
+export default Login;
