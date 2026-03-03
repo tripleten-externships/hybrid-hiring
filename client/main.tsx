adc0e9b6410e356b5550b1b70f3e1485b28c345f
github.com/GarionMorgan <garmorgan14@yahoo.com>
2026-03-03 13:45:55 -0600

    [HH-40] Create Page Not Found page

--- client/main.tsx
+++ client/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/App';
---
+++ imports/ui/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import './NotFoundPage.css';
+
+export const NotFoundPage = () => {
+  return (
+    <div className="NotFound__content">
+      <h2>404</h2>
+      <h3>Page Not Found</h3>
+      <p>
+        <a href="/">Return Home</a>
+      </p>
+    </div>
+  );
+};
