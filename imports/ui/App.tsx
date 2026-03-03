2c4af1ed67b4e776866a1ef8d9f0a85404821822
Alaa Rajab <alaa88rajab@gmail.com>
2026-03-03 01:17:19 -0600

    [HH-45] Build accessible TextInput component

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
+import TextInput from './components/TextInput';
 import { Hello } from './Hello';
 import { Info } from './Info';
 import { UsersList } from './UsersList';
 import { UsersManager } from './UsersManager';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-    <hr />
-    <UsersList />
-    <hr />
-    <UsersManager />
-  </div>
-);
+export const App = () => {
+  const [email, setEmail] = useState('');
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <Hello />
+      <Info />
+      <hr />
+      <UsersList />
+      <hr />
+      <UsersManager />
+<TextInput
+        label="Email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        error={!email ? 'Required' : undefined}
+      />
+    </div>
+      
+    
+  );
+};
