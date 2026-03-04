4fd85bd4122776531c4a86b0128029624eb02df8
Alaa Rajab <alaa88rajab@gmail.com>
2026-03-03 22:15:22 -0600

    [HH-46] Fix Prettier formatting

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -5,26 +5,26 @@ import { UsersList } from './UsersList';
 import { UsersManager } from './UsersManager';
 import TextArea from './components/TextArea';
 
-export const App = () => { 
-   const [message, setMessage] = useState('');
-  return(
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-    <hr />
-    <UsersList />
-    <hr />
-    <UsersManager />
-    <TextArea
+export const App = () => {
+  const [message, setMessage] = useState('');
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <Hello />
+      <Info />
+      <hr />
+      <UsersList />
+      <hr />
+      <UsersManager />
+      <TextArea
         label="Message"
         id="message"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
-        rows={10} 
-        error={!message ? 'Required' : undefined} 
+        rows={10}
+        error={!message ? 'Required' : undefined}
         fullWidth
       />
-  </div>
-);
+    </div>
+  );
 };
