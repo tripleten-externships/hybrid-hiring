fc8e0110e6aa06f704c4529274cef6aea9514f97
Alaa Rajab <alaa88rajab@gmail.com>
2026-03-03 22:04:28 -0600

    [HH-46] Build TextArea component with rows and error support

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import { Hello } from './Hello';
 import { Info } from './Info';
 import { UsersList } from './UsersList';
 import { UsersManager } from './UsersManager';
+import TextArea from './components/TextArea';
 
-export const App = () => (
+export const App = () => { 
+   const [message, setMessage] = useState('');
+  return(
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello />
@@ -12,5 +16,15 @@ export const App = () => (
     <UsersList />
     <hr />
     <UsersManager />
+    <TextArea
+        label="Message"
+        id="message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        rows={10} 
+        error={!message ? 'Required' : undefined} 
+        fullWidth
+      />
   </div>
 );
+};
