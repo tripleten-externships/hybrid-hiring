1d65b7dd02038400c8eb1bbf694636eaa9dc062c
holden <james.holden.moore@gmail.com>
2026-03-04 12:12:44 -0500

    [HH-44] resolving merge conflicts

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,18 +1,4 @@
-import { Hello } from './Hello';
-import { Info } from './Info';
-import { UsersList } from './UsersList';
-import { UsersManager } from './UsersManager';
-import { Button } from './components/Button';
+import { Router } from './router';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-    <hr />
-    <UsersList />
-    <hr />
-    <UsersManager />
-      <Button>Click me</Button>
-  </div>
-);
+export const App = () => <Router />;
+
