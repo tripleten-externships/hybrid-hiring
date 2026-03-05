d45fc31577f5757673454773598366c611b1bbfd
RyanStoll <pushthatboard@gmail.com>
2026-03-04 20:48:47 -0600

    [HH-56] create login form outline

--- imports/ui/pages/Login/Login.tsx
+++ imports/ui/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Layout } from '../components/Layout.tsx';
-import "./Login.css";
+import './Login.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -10,34 +10,40 @@ const Login = () => {
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
-return (
+  return (
     <Layout>
-<form className='login__form' >
-    <h2 className="login__form-title">Welcome back or LogIn to your account</h2>
-    <label htmlFor='email' className='login__form-label'>Email</label>
-    <input 
-        type='email'
-        id='email'
-        className='login__form-input'
-        value={email}
-        placeholder='Enter your email'
-        onChange={e => setEmail(e.target.value)}
-        required
-    />
-    <label htmlFor='password' className='login__form-label'>Password</label>
-    <input 
-        type={showPassword ? 'text' : 'password'}
-        id='password'
-        className='login__form-input'
-        value={password}
-        placeholder='Enter your password'
-        onChange={e => setPassword(e.target.value)}
-        required
-    />
-</form>
-    <h3 className="login__signup-btn">Don't have an account? <a href="/signup">Sign up</a></h3>
+      <form className="login__form">
+        <h2 className="login__form-title">Welcome back or LogIn to your account</h2>
+        <label htmlFor="email" className="login__form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="login__form-input"
+          value={email}
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password" className="login__form-label">
+          Password
+        </label>
+        <input
+          type={showPassword ? 'text' : 'password'}
+          id="password"
+          className="login__form-input"
+          value={password}
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </form>
+      <h3 className="login__signup-btn">
+        Don't have an account? <a href="/signup">Sign up</a>
+      </h3>
     </Layout>
-    );
+  );
 };
 
 export default Login;
