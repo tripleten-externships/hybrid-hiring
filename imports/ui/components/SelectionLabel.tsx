e081103de82e30c9eccaf5231b7566ab9dc2ef09
holden <james.holden.moore@gmail.com>
2026-03-05 13:07:55 -0500

    HH-47 SelectionLabel.tsx

---
+++ imports/ui/components/SelectionLabel.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type SelectionLabelProps = {
+  label: string;
+  selected: boolean;
+  onClick: () => void;
+};
+
+export default function SelectionLabel({ label, selected, onClick }: SelectionLabelProps) {
+  return (
+    <button
+      type="button"
+      className={`chip ${selected ? 'chip--selected' : ''}`}
+      aria-pressed={selected}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+}
