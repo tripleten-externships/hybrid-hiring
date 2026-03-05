11c628911e970be9ffd8dacd467ab338ecf14df4
holden <james.holden.moore@gmail.com>
2026-03-05 16:48:34 -0500

    JobCard.tsx

--- imports/ui/components/JobCard.tsx
+++ imports/ui/components/JobCard.tsx
@@ -1,54 +1,71 @@
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
+import SelectionLabel from './SelectionLabel';
 
 type Job = {
-
-    _id:string;
-    title:string;
-    company:string;
-    location:string;
-    basePay:number;
-    payMax?:number;
-    payUnit:string;
-    jobTyoe:string;
-    tags?:string[];
-
+  _id: string;
+  title: string;
+  company: string;
+  location: string;
+  basePay: number;
+  payMax?: number;
+  payUnit: string;
+  jobType: string;
+  tags?: string[];
 };
 
+type JobCardProps = {
+  job: Job;
+  isSaved?: boolean;
+  onSave?: () => void;
+};
 
-export default function JobCard({job, isSaved, onSave}: JobCardProps){
+export default function JobCard({ job, isSaved, onSave }: JobCardProps) {
+  const firstLetter = job.company.charAt(0).toUpperCase();
 
-    const firstLetter = job.company.charAt(0).toUppercase();
-
-    const pay = 
-    job.payMax
-    ? `$${job.basePay}-${job.payMax}/${job.payUnit}`
+  const pay = job.payMax
+    ? `$${job.basePay}–${job.payMax}/${job.payUnit}`
     : `$${job.basePay}/${job.payUnit}`;
 
-    return(
-        <div className="job-card__badge">
-            {firstLetter}
+  return (
+    <div className="job-card">
+      {/* Company Badge */}
+      <div className="job-card__badge">{firstLetter}</div>
+
+      <div className="job-card__content">
+        {/* Title       */}
+        <Link to={`/jobs/${job._id}`} className="job-card__title">
+          {job.title}
+        </Link>
+
+        {/* Company */}
+        <div className="job-card__company">{job.company}</div>
+
+        {/* Location */}
+        <div className="job-card__location">{job.location}</div>
+
+        {/* Pay */}
+        <div className="job-card__pay">{pay}</div>
+
+        {/* Job Type Chip */}
+        <SelectionLabel label={job.jobType} />
+
+        {/* Tags */}
+        <div className="job-card__tags">
+          {job.tags?.map((tag) => (
+            <span key={tag} className="job-card__tag">
+              {tag}
+            </span>
+          ))}
         </div>
+      </div>
 
-        <div className="job-card__content">
-{/* Title       */}
-<Link to={`/jobs/${job._id}`} className="job-card__title">
-{job.title}
-</Link>
-
-{/* Company */}
-{/* Location */}
-{/* Pay */}
-{/* Job Type Chip */}
-{/* Tags */}
-{/* Bookmark */}
-
-
-
-
-  </div>
-
-
-    );
-
-
+      {/* Bookmark */}
+      {onSave && (
+        <button
+          className={`job-card__bookmark ${isSaved ? 'saved' : ''}`}
+          onClick={onSave}
+        >BUTTON</button>
+      )}
+    </div>
+  );
 }
