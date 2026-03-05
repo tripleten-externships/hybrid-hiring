6d69655027f7a61a50598b7429899a8f8e0b4311
Scarylyn <scarylyn@gmail.com>
2026-03-05 05:27:53 -0500

    [HH-50] btn base styles done

--- imports/ui/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-    return (
-        <div>
-            <h1>This is the Footer</h1>
-        </div>
-    )
-}
---
+++ imports/ui/layouts/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div>
+      <h1>This is the Footer</h1>
+    </div>
+  );
+};
