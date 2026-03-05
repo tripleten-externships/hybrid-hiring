416c5223b2d3efc1457a2649c1257a3508ed2982
Scarylyn <scarylyn@gmail.com>
2026-03-05 06:12:39 -0500

    [HH-50] moved a file

--- imports/ui/router.tsx
+++ imports/ui/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Layout } from './layouts/Layout';
+import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { DemoUsersList } from './examples/DemoUsersList';
 import { DemoUsersManager } from './examples/DemoUsersManager';
