897d6733f02e37647561fabf16921a39b34021e0
Alaa Rajab <alaa88rajab@gmail.com>
2026-03-05 01:44:42 -0600

    [HH-46] add TextArea example usage

--- imports/ui/pages/Home.tsx
+++ imports/ui/pages/Home.tsx
@@ -1,13 +1,30 @@
+import React, { useState } from 'react';
 import { Info } from '../examples/Info';
 import { Hello } from '../examples/Hello';
+import TextArea from '../components/TextArea';
 
 export const Home = () => {
+  const [message, setMessage] = useState('');
+
   return (
     <div>
       <h1>Welcome to Hybrid Hiring Solutions!</h1>
+
       <Hello />
       <br />
+
       <Info />
+      <br />
+
+      <TextArea
+        label="Message"
+        id="message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        rows={10}
+        error={!message ? 'Required' : undefined}
+        fullWidth
+      />
     </div>
   );
 };
