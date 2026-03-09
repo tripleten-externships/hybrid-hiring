fb34b10439c035c9be0f66d28da3a089e92ed28c
Alaa Rajab <alaa88rajab@gmail.com>
2026-03-09 16:42:50 -0500

    [HH-45] Add TextInput example on Home page for testing

--- imports/ui/pages/Home.tsx
+++ imports/ui/pages/Home.tsx
@@ -1,13 +1,23 @@
 import { Info } from '../examples/Info';
 import { Hello } from '../examples/Hello';
+import TextInput from '../components/TextInput';
+import { useState } from 'react';
 
 export const Home = () => {
+  const [email, setEmail] = useState('');
   return (
     <div>
       <h1>Welcome to Hybrid Hiring Solutions!</h1>
       <Hello />
       <br />
       <Info />
+      <TextInput
+  label="Email"
+  id="email"
+  value={email}
+  onChange={(e) => setEmail(e.target.value)}
+  error={!email ? 'Required' : undefined}
+/>
     </div>
   );
 };
