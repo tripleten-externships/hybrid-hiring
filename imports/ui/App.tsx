042bad3e3d2678dbfbcd134ed44434f7c8c285d6
alaarajab <125327259+alaarajab@users.noreply.github.com>
2026-03-09 15:47:04 -0500

    Resolve merge conflict in App.tsx with main (use Router)
    Refactored App component to use Router instead of multiple components.

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,29 +1,3 @@
-import { useState } from 'react';
-import TextInput from './components/TextInput';
-import { Hello } from './Hello';
-import { Info } from './Info';
-import { UsersList } from './UsersList';
-import { UsersManager } from './UsersManager';
+import { Router } from './router';
 
-export const App = () => {
-  const [email, setEmail] = useState('');
-
-  return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
-      <Hello />
-      <Info />
-      <hr />
-      <UsersList />
-      <hr />
-      <UsersManager />
-      <TextInput
-        label="Email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        error={!email ? 'Required' : undefined}
-      />
-    </div>
-  );
-};
+export const App = () => <Router />;
