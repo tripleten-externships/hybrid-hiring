8da5b2b85076ba69674a957aca49858bdf21fb37
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2026-03-09 14:13:55 -0500

    [HH-63] implement Profiles.upsert method

--- imports/api/profiles/methods.ts
+++ imports/api/profiles/methods.ts
@@ -1,5 +1,32 @@
 import { Meteor } from 'meteor/meteor';
 import { ProfilesCollection } from './collection';
+import { check, Match } from 'meteor/check';
 
-// methods will be added here
-Meteor.methods({});
+const requireUser = (userId: string | null): string => {
+  if (!userId) {
+    throw new Meteor.Error('not-authorized', 'Login required');
+  }
+  return userId;
+};
+
+Meteor.methods({
+  async 'Profiles.upsert'(data) {
+    const userId = requireUser(this.userId);
+
+    check(data, Match.ObjectIncluding({}));
+    const profile = await ProfilesCollection.findOneAsync({ userId });
+
+    if (profile) {
+      await ProfilesCollection.updateAsync(profile._id, {
+        $set: { ...data, updatedAt: new Date() },
+      });
+    } else {
+      await ProfilesCollection.insertAsync({
+        userId,
+        ...data,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+  },
+});
