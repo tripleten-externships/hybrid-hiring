2be097c5cbd4a94d9fa822c6bf75d35dc6ebf1e4
Scarylyn <scarylyn@gmail.com>
2026-03-11 19:22:41 -0400

    [HH-50] added quick links, made title clickable

--- imports/ui/layouts/Footer.tsx
+++ imports/ui/layouts/Footer.tsx
@@ -10,27 +10,46 @@ export const Footer = () => {
       <div className="footer__content">
         <div className="footer__brand">
           <img
-            src="./assets/hhr-logo.svg"
+            src="./assets/company-logo.svg"
             alt="Hybrid Hiring Solutions Logo"
             className="footer__logo"
           />
-          <h2 className="footer__title">Hybrid Hiring Solutions</h2>
+          <NavLink to="/" className="footer__title-nav">
+            <h2 className="footer__title">Hybrid Hiring Solutions</h2>
+          </NavLink>
         </div>
         <nav className="footer__nav">
-          <NavLink to="/" className="footer__nav-link">Home</NavLink>
+          <p className="footer__nav-link footer__nav-link-title">
+            Quick Links
+          </p>
           {/* Employers is in the Figma design but not explicitly in the task spec */}
-          <NavLink to="/employers" className="footer__nav-link">Employers</NavLink>
-          <NavLink to="/jobs" className="footer__nav-link">Jobs</NavLink>
-          <NavLink to="/contact" className="footer__nav-link">Contact Us</NavLink>
-          <NavLink to="/about" className="footer__nav-link">About Us</NavLink>
+          <NavLink to="/employers" className="footer__nav-link">
+            Employers
+          </NavLink>
+          <NavLink to="/jobs" className="footer__nav-link">
+            Jobs
+          </NavLink>
+          <NavLink to="/contact" className="footer__nav-link">
+            Contact Us
+          </NavLink>
+          <NavLink to="/about" className="footer__nav-link">
+            About Us
+          </NavLink>
           {/* Figma shows an "account" button; task spec also calls for login/signup */}
-          {/* Shows Signup/Login when logged out, Account when logged in */}
-          {loggedIn ? (<NavLink to="/account" className="footer__nav-link">Account</NavLink>)
-            : (<>
-              <NavLink to="/signup" className="footer__nav-link">Sign Up</NavLink>
-              <NavLink to="/login" className="footer__nav-link">Log In</NavLink>
+          {loggedIn ? (
+            <NavLink to="/account" className="footer__nav-link">
+              Account
+            </NavLink>
+          ) : (
+            <>
+              <NavLink to="/signup" className="footer__nav-link">
+                Sign Up
+              </NavLink>
+              <NavLink to="/login" className="footer__nav-link">
+                Log In
+              </NavLink>
             </>
-            )}
+          )}
         </nav>
         <div className="footer__socials">
           {/* TODO: replace href values with real social URLs when available */}
