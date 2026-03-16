57b3dd42d4f5c687c0a364a1702c33e63d73c06d
holden <james.holden.moore@gmail.com>
2026-03-16 13:30:03 -0400

    [HH-25] boiler plate

---
+++ imports/ui/components/JobBoard/JobBoard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Button } from '/imports/ui/components/Button/Button';
+// import JobCard from '/imports/ui/components/JobCard/jobCard';
+import { JobsCollection } from '/imports/api/jobs';
+
+import '.JobBoard.css';
+
+// type Job = {};
+
+// export default function JobBoard () {
+
+//     return();
+// };
