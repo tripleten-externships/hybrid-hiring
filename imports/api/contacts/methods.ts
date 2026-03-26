59c982fa3e1405cd3d716c476f98b9cb9ead37f8
Alaa Rajab <alaa88rajab@gmail.com>
2026-03-26 13:07:51 -0500

    [HH-101] Implement contacts.submit and contacts.remove methods

---
+++ imports/api/contacts/methods.ts
@@ -0,0 +1,39 @@
+import { Meteor } from 'meteor/meteor';
+import { ContactsCollection } from './collection';
+import { requireAdmin } from '/imports/api/admin/collection';
+
+
+async function submit(this: Meteor.MethodThisType, data: {
+  firstName: string;
+  lastName?: string;
+  email: string;
+  phone?: string;
+  message: string;
+}) {
+  const { firstName, lastName, email, phone, message } = data;
+
+  if (!firstName || !email || !message) {
+    throw new Meteor.Error('validation-error', 'Missing required fields');
+  }
+
+  return await ContactsCollection.insertAsync({
+    firstName,
+    lastName: lastName || '',
+    email,
+    phone,
+    message,
+    submittedAt: new Date(),
+  });
+}
+
+
+async function remove(this: Meteor.MethodThisType, contactId: string) {
+  requireAdmin(this.userId ?? undefined);
+
+  return await ContactsCollection.removeAsync({ _id: contactId });
+}
+
+Meteor.methods({
+  'contacts.submit': submit,
+  'contacts.remove': remove,
+});
