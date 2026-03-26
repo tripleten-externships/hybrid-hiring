b5e7de00fbb4cf25783d4358d8af27797238adf6
holden <james.holden.moore@gmail.com>
2026-03-26 17:26:19 -0400

    [HH-25] subtask hh-85, job-board styles

--- imports/ui/pages/Jobs/Jobs.tsx
+++ imports/ui/pages/Jobs/Jobs.tsx
@@ -7,7 +7,7 @@ export const Jobs = () => {
       <h2>Hi, I'm the Jobs page!</h2>
       <p>You can only see me if you're logged in! 😏</p>
       <p>There will be jobs here someday...</p>
-      <JobBoard/>
+      <JobBoard />
     </div>
   );
 };
