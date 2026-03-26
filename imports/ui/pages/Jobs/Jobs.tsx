a0c110ec2857ba08be81aeadb989a074aed49786
holden <james.holden.moore@gmail.com>
2026-03-26 18:13:46 -0400

    [HH-25] subtask hh-85, JobBoard PR

--- imports/ui/pages/Jobs/Jobs.tsx
+++ imports/ui/pages/Jobs/Jobs.tsx
@@ -6,7 +6,6 @@ export const Jobs = () => {
       <h2>Hi, I'm the Jobs page!</h2>
       <p>You can only see me if you're logged in! 😏</p>
       <p>There will be jobs here someday...</p>
-      
     </div>
   );
 };
