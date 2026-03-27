9a44b358cbdd08d1a48fbfedb98624c72ab7b7e7
Glen Watson <watsonglen4@gmail.com>
2026-03-27 13:23:26 -0700

    [HH-81] refactor job types

--- imports/api/profiles/collection.ts
+++ imports/api/profiles/collection.ts
@@ -1,11 +1,12 @@
 import { Mongo } from 'meteor/mongo';
+import { JobType } from '../../types/jobs';
 
 export interface UserProfile {
   _id?: string;
   userId: string;
   name?: string;
   role?: 'admin' | 'user';
-  jobTypes?: string[];
+  jobTypes?: JobType[];
   minPay?: number;
   payUnit?: 'hourly' | 'yearly';
   city?: string;
