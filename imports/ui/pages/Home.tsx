53fb6efda93800b9f8679d994882c039e9b1151b
holden <james.holden.moore@gmail.com>
2026-04-06 13:56:11 -0400

    [HH-82] Removed JobCard testing on home.tsx

--- imports/ui/pages/Home.tsx
+++ imports/ui/pages/Home.tsx
@@ -3,25 +3,7 @@ import { Info } from '../examples/Info';
 import { Hello } from '../examples/Hello';
 import { TextArea, TextInput } from '../components';
 
-//
-import JobCard from '../components/JobCard/JobCard';
-
-// JobCard Test-----
-const testJob = {
-  _id: '1',
-  title: 'Master Electrician',
-  company: 'Trinity Solar',
-  location: 'Pittsburgh, PA 15201 (Central Lawrenceville area',
-  basePay: 46,
-  payMax: 50,
-  payUnit: 'hr',
-  jobType: 'Full-time',
-  tags: ['Paid Training', '401(K) matching'],
-};
-
-//
 export const Home = () => {
-  const [saved, setSaved] = useState(false);
 
   const [message, setMessage] = useState('');
   const [email, setEmail] = useState('');
@@ -37,8 +19,6 @@ export const Home = () => {
   return (
     <div>
       <h1>Welcome to Hybrid Hiring Solutions!</h1>
-      {/* JobCard Test */}
-      <JobCard job={testJob} isSaved={saved} onSave={() => setSaved((prev) => !prev)} />
 
       <Hello />
       <br />
