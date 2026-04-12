3249d75eb217de696da1841eda3fbd04c52f5291
Alaa Rajab <alaa88rajab@gmail.com>
2026-04-12 17:24:45 -0500

    [HH-100] Remove console.log from contacts collection

--- imports/api/contacts/collection.ts
+++ imports/api/contacts/collection.ts
@@ -11,4 +11,4 @@ export interface ContactSubmission {
 }
 
 export const ContactsCollection = new Mongo.Collection<ContactSubmission>('contacts');
-console.log('ContactsCollection initialized:', ContactsCollection);
+
