b3176cd2fef2c9f236aab4e0346ed06973be7047
holden <james.holden.moore@gmail.com>
2026-04-21 18:38:30 -0400

    [HH-94] mobile styles, re-structure AboutUs.tsx to seperate content blocks

--- imports/ui/pages/AboutUs/AboutUs.tsx
+++ imports/ui/pages/AboutUs/AboutUs.tsx
@@ -4,12 +4,15 @@ export const AboutUs = () => {
   return (
     <main className="about-us">
       <section className="about-hero">
-        {/* Desktop only overlay, gradiant left-->right */}
+
+        {/* Desktop */}
         <div className="about-hero__overlay" aria-hidden="true" />
-        <div className="about-hero__mobile-img" aria-hidden="true" />
-        <div className="about-hero__content">
+
+        <div className="about-hero__content about-hero__content--desktop">
+
           <h1 className="about-hero__headline">About Hybrid Hiring Solutions</h1>
-          <p className="about-hero__body about-hero__body--desktop">
+
+          <p className="about-hero__body">
             Hybrid Hiring Solutions is a staffing company located in NE Pennsylvania that provides a
             personalized experience to search and source candidates for job openings clients have.
             The organization largely supports clients in the gas and energy industry that have jobs
@@ -17,21 +20,33 @@ export const AboutUs = () => {
             focuses often require specialized skills such as chemists, equipment operators, and
             financial analysts.
           </p>
-          <p className="about-hero__body about-hero__body--mobile">
+          </div>
+
+          {/* Mobile */}
+          <div className="about-hero__mobile-top">
+
+            <h1 className="about-hero__headline">About Hybrid Hiring Solutions</h1>
+                    </div>
+
+<div className="about-hero__mobile-body">
+          <p className="about-hero__body">
             Hybrid Hiring Solutions is a staffing company located in NE Pennsylvania that provides a{' '}
             <strong className="about-hero__emphasis">personalized experience </strong>
             to search and source candidates for job openings clients have.
           </p>
-          <p className="about-hero__body about-hero__body--mobile">
+          <p className="about-hero__body">
             {' '}
+            
             The organization largely supports clients in the gas and energy industry that have jobs
             in Northeastern Pennsylvania as well as globally.
           </p>
-          <p className="about-hero__body about-hero__body--mobile">
+          <p className="about-hero__body">
+           {' '}
             The jobs Hybrid Hiring Solutions focuses often require specialized skills such as
             chemists, equipment operators, and financial analysts.
           </p>
         </div>
+
       </section>
     </main>
   );
