6078da634b4aaf04dde06f1eb7370a116ab354bd
holden <james.holden.moore@gmail.com>
2026-04-21 15:44:11 -0400

    [HH-94] add AboutUs.tsx

--- imports/ui/pages/AboutUs/AboutUs.tsx
+++ imports/ui/pages/AboutUs/AboutUs.tsx
@@ -1,21 +1,22 @@
 import './AboutUs.css';
 
 export const AboutUs = () => {
-return (
+  return (
     <main className="about-us">
-        <section className="about-hero">
-            <div className="about-hero__overlay">
-                <div className="about-hero__mobile-img">
-                    <div className="about-hero__content">
-                        <h1 className="about-hero__headline"></h1>
-                        <p className="about-hero__body">
-                            <strong className="about-hero__emphasis"></strong></p>
-                            <p className="about-hero__body"></p>
-                            <p className="about-hero__body"></p>
-                            </div>
-                </div>
+      <section className="about-hero">
+        <div className="about-hero__overlay">
+          <div className="about-hero__mobile-img">
+            <div className="about-hero__content">
+              <h1 className="about-hero__headline"></h1>
+              <p className="about-hero__body">
+                <strong className="about-hero__emphasis"></strong>
+              </p>
+              <p className="about-hero__body"></p>
+              <p className="about-hero__body"></p>
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
     </main>
-)
+  );
 };
