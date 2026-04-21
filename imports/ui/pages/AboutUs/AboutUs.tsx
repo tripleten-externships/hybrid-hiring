6024a6b4ce02b2c5080d9619182b422951cd897c
holden <james.holden.moore@gmail.com>
2026-04-21 15:43:07 -0400

    [HH-94] AboutUs.tsx boilerplate

---
+++ imports/ui/pages/AboutUs/AboutUs.tsx
@@ -0,0 +1,21 @@
+import './AboutUs.css';
+
+export const AboutUs = () => {
+return (
+    <main className="about-us">
+        <section className="about-hero">
+            <div className="about-hero__overlay">
+                <div className="about-hero__mobile-img">
+                    <div className="about-hero__content">
+                        <h1 className="about-hero__headline"></h1>
+                        <p className="about-hero__body">
+                            <strong className="about-hero__emphasis"></strong></p>
+                            <p className="about-hero__body"></p>
+                            <p className="about-hero__body"></p>
+                            </div>
+                </div>
+            </div>
+        </section>
+    </main>
+)
+};
